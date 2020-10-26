e6fdcd8649cce8d0f95b7d3b2d63b7c6f00469ac
Ethan Tom <etom579@gmail.com>
2020-10-26 00:53:53 -0700

    env vars setup

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './pages/HomeScreen';
-import SettingsScreen from './pages/SettingsScreen';
+import HomeScreen from './src/pages/HomeScreen';
+import SettingsScreen from './src/pages/SettingsScreen';
+import LoginPage from './src/pages/LoginPage';
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
 	return (
 		<NavigationContainer>
 			<Tab.Navigator>
+				<Tab.Screen name="Login" component={LoginPage} />
 				<Tab.Screen name="Home" component={HomeScreen} />
 				<Tab.Screen name="Settings" component={SettingsScreen} />
 			</Tab.Navigator>
