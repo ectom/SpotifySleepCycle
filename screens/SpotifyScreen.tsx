8c1900554b6d81aa8c820822474d7cbc350bf286
Ethan Tom <etom579@gmail.com>
2020-11-05 00:03:21 -0800

    user login successful. need proper ordered flow of token, auth, data, and logout

--- screens/SpotifyScreen.tsx
+++ screens/SpotifyScreen.tsx
@@ -1,15 +1,60 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
+import React, { useState, useEffect } from 'react';
+import { Platform, StyleSheet, Button } from 'react-native';
+import api from '../api/spotifyApi';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import * as Linking from 'expo-linking';
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import * as SecureStore from 'expo-secure-store';
+
+
+
+const discovery = {
+  authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+  tokenEndpoint: 'https://accounts.spotify.com/api/token',
+};
 
 const SpotifyScreen = () => {
+
+  // These will hold info from API such as token, genre, etc
+  const [token, setToken] = useState('');
+  useEffect(() => {
+
+  }, []);
+  const [genres, setGenres] = useState({ selectedGenre: '', listOfGenresFromAPI: [] });
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: `${process.env.REACT_NATIVE_SPOTIFY_ID}`,
+      scopes: ['user-read-email', 'playlist-modify-public'],
+      usePKCE: false,
+      redirectUri: makeRedirectUri({
+        native: Linking.makeUrl(),
+      }),
+    },
+    discovery
+  );
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const auth = response.params;
+      if (Platform.OS !== 'web') {
+        SecureStore.setItemAsync("AUTH_CODE", auth.code);
+        SecureStore.setItemAsync("AUTH_STATE", auth.state);
+      }
+    }
+  }, [response]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Spotify</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/SpotifyScreen.js" />
+      <Button
+        onPress={() => { promptAsync() }}
+        disabled={!request}
+        title="Login"
+        color="#1DB954"
+      />
+      {/* <Text>User token: {token}</Text> */}
     </View>
   );
 }
